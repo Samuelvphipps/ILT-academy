40c9ff7bb501fac39ff5970de9a2529dc0a2485d
Sam Phipps <samuel.v.phipps@gmail.com>
2023-01-12 12:44:22 -0600

    new pool

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -11,21 +11,12 @@ const url = require('url');
 let config = {};
 
 if (process.env.DATABASE_URL) {
-  // Heroku gives a url, not a connection object
-  // https://github.com/brianc/node-pg-pool
-  const params = url.parse(process.env.DATABASE_URL);
-  const auth = params.auth.split(':');
-
-  config = {
-    user: auth[0],
-    password: auth[1],
-    host: params.hostname,
-    port: params.port,
-    database: params.pathname.split('/')[1],
-    ssl: { rejectUnauthorized: false },
-    max: 10, // max number of clients in the pool
-    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-  };
+    pool = new pg.Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
 } else {
   config = {
     host: 'localhost', // Server hosting the postgres database
