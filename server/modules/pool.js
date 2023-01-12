b55857457e52db0797e5ab9c09bbee7c28c56261
Sam Phipps <samuel.v.phipps@gmail.com>
2023-01-12 12:49:52 -0600

    heroku fix

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -29,10 +29,6 @@ else {
     });
 }
 
-module.exports = pool;
-
-// the pool with emit an error on behalf of any idle clients
-// it contains if a backend error or network partition happens
 pool.on('error', (err) => {
   console.log('Unexpected error on idle client', err);
   process.exit(-1);
